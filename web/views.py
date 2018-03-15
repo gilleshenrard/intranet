f2abfe596d3ef5c626af656ce66e32e9a089c4fb
Gilles Henrard <gilles.henrard@hotmail.com>
2018-03-15 11:51:03 +0100

    implement base home page with list of all users

--- web/views.py
+++ web/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import render
 from django.http.response import HttpResponseNotFound
+from requests import get
+from rest_framework import status
+from django.core.urlresolvers import reverse
 import logging
 
 stdlogger = logging.getLogger(__name__)
@@ -6,13 +10,19 @@ stdlogger = logging.getLogger(__name__)
 
 def home(request):
     """Handles home page behaviour and rendering"""
-
     stdlogger.info("HOME : request for home page")
-    return HttpResponseNotFound()
+
+    r = get("http://localhost:8000" + reverse("get_post_profiles"))
+    if r.status_code == status.HTTP_200_OK:
+        stdlogger.info("HOME : All profiles successfully retrieved.")
+        return render(request, 'web/home.html', {'users': r.json()})
+    else:
+        stdlogger.error("HOME : Profiles could not be found.")
+        return HttpResponseNotFound("Profiles not found.")
 
 
 def profile(request, usrname):
     """Handles profile page behaviour and rendering"""
 
-    stdlogger.info("profile : request for profile of '" + usrname + "'")
+    stdlogger.info("PROFILE : request for profile of '" + usrname + "'")
     return HttpResponseNotFound()
