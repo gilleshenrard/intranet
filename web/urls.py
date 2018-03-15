1cc37a38d78f207ffa3780e078da22f9bf31a45a
Gilles Henrard <gilles.henrard@hotmail.com>
2018-03-15 13:54:26 +0100

    implement base user authentication mechanism

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'profile/(?P<usrname>[A-z -]+)$', views.profile, name="profile"),
+    url(r'^logout', views.user_logout, name="logout"),
 ]
--- web/views.py
+++ web/views.py
@@ -3,6 +3,10 @@ from django.http.response import HttpResponseNotFound
 from requests import get
 from rest_framework import status
 from django.core.urlresolvers import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 import logging
 
 stdlogger = logging.getLogger(__name__)
@@ -12,13 +16,44 @@ def home(request):
     """Handles home page behaviour and rendering"""
     stdlogger.info("HOME : request for home page")
 
+    if request.method == "POST":
+        # login form sent
+        stdlogger.info("HOME_post : login request received")
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            # login data is valid
+            stdlogger.debug("HOME_post : login form data is valid")
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                stdlogger.info("HOME_post : user '" + username + "' successfully authenticated")
+                messages.add_message(request, messages.SUCCESS, 'You are now authenticated')
+
+        else:
+            # login data is invalid
+            stdlogger.error("HOME_post : login form data is invalid")
+            for field, errors in form.errors.items():
+                for error in errors:
+                    stdlogger.error(field + " -> " + error)
+                    messages.add_message(request, messages.ERROR, field + " : " + error)
+
     r = get("http://localhost:8000" + reverse("get_post_profiles"))
     if r.status_code == status.HTTP_200_OK:
-        stdlogger.info("HOME : All profiles successfully retrieved.")
+        stdlogger.info("HOME_get : All profiles successfully retrieved.")
         return render(request, 'web/home.html', {'users': r.json()})
     else:
-        stdlogger.error("HOME : Profiles could not be found.")
+        stdlogger.error("HOME_get : Profiles could not be found.")
         return HttpResponseNotFound("Profiles not found.")
+
+
+def user_logout(request):
+    """Handles user logout and redirects to home page"""
+    logout(request)
+    return redirect(reverse('home'))
 
 
 def profile(request, usrname):
