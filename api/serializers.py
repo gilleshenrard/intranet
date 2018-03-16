42b88d006cdc32a58749059331d766679e4fe82f
Gilles Henrard <gilles.henrard@hotmail.com>
2018-03-16 19:40:34 +0100

    override save() and update() in UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,22 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = '__all__'
 
+    def save(self, **kwargs):
+        """Ensures the password, if provided, is hashed before saving"""
+
+        super(UserSerializer, self).save(**kwargs)
+
+        if "password" in self.validated_data:
+            self.instance.set_password(self.validated_data['password'])
+            self.instance.save()
+
+    def update(self, instance, validated_data):
+        """Ensures the password, if provided, is hashed before updating"""
+
+        super(UserSerializer, self).update(instance, validated_data)
+
+        if "password" in self.validated_data:
+            instance.set_password(self.validated_data['password'])
+            instance.save()
+
+        return instance
